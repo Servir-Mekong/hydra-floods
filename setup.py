0d6cdd7aeb437d2adce98847f697076a67ac57f1
loaner <loaner@ServirMacLoan-5057592.local>
2018-10-15 04:02:47 -0500

    adding setup.py file for hydrafloods package

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='hydrafloods',
+      version='0.0.1',
+      description='HYDrologic Remote sensing Analysis for Floods',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/servir-mekong/hydra-floods',
+      packages=setuptools.find_packages(),
+      author='Kel Markert',
+      author_email='kel.markert@gmail.com',
+      license='MIT',
+      zip_safe=False)
