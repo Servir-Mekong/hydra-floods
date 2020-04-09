1ccb6f1f5d030905b50c7d1a33bec75019925a97
kmarkert <kel.markert@gmail.com>
2020-04-09 16:30:15 -0500

    expanding gee export functionality with support for collections

--- hydrafloods/geeutils.py
+++ hydrafloods/geeutils.py
@@ -42,27 +42,64 @@ def getTileLayerUrl(ee_image_object):
     return tile_url_template.format(**map_id)
 
 
-def exportImage(image,region,assetId,description=None,scale=90,crs='EPSG:4326'):
+def exportImage(image, region, assetId, description=None, scale=1000, crs='EPSG:4326', pyramiding=None):
     if (description == None) or (type(description) != str):
         description = ''.join(random.SystemRandom().choice(
-        string.ascii_letters) for _ in range(8)).lower()
+            string.ascii_letters) for _ in range(8)).lower()
     # get serializable geometry for export
     exportRegion = region.bounds().getInfo()['coordinates']
 
+    if pyramiding is None:
+        pyramiding = {'.default': 'mean'}
+
     # set export process
     export = ee.batch.Export.image.toAsset(image,
-      description = description,
-      assetId = assetId,
-      scale = scale,
-      region = exportRegion,
-      maxPixels = 1e13,
-      crs = crs
-    )
+                                           description=description,
+                                           assetId=assetId,
+                                           scale=scale,
+                                           region=exportRegion,
+                                           maxPixels=1e13,
+                                           crs=crs,
+                                           pyramidingPolicy=pyramiding
+                                           )
     # start export process
     export.start()
 
     return
 
+
+def batchExport(collection, region, collectionAsset, prefix=None, suffix=None, scale=1000, crs='EPSG:4326', metadata=None, pyramiding=None):
+    n = collection.size()
+    exportImages = collection.sort('system:time_start', False).toList(n)
+    nIter = n.getInfo()
+
+    for i in range(nIter):
+        img = ee.Image(exportImages.get(i))
+        if metadata is not None:
+            img = img.set(metadata)
+
+        t = img.get('system:time_start').getInfo()
+        date = datetime.datetime.utcfromtimestamp(
+            t / 1e3).strftime("%Y%m%d")
+
+        exportName = date
+        if prefix is not None:
+            exportName = f"{prefix}_" + exportName
+        if suffix is not None:
+            exportName = exportName + f"_{suffix}"
+
+        description = exportName
+        print(f"running export for {description}")
+
+        if not collectionAsset.endswith('/'):
+            collectionAsset += '/'
+
+        exportName=collectionAsset + description
+
+        exportImage(img, region, exportName, description=description,
+                    scale=scale, crs=crs, pyramiding=pyramiding)
+
+    return
 
 def rescaleBands(img):
     def individualBand(b):
