96561eb304f4443c22382c12649ea514edacdf16
kmarkert <kel.markert@gmail.com>
2020-05-06 05:25:14 -0500

    added comments and forced metadata wrapper dtypes

--- hydrafloods/decorators.py
+++ hydrafloods/decorators.py
@@ -6,7 +6,8 @@ def carryMetadata(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         # expects first element of args is img
-        result = func(*args,**kwargs)
+        # this assumption is true for 99.9% of fuctions used for ee.ImageCollection.map()
+        result = ee.Image(func(*args,**kwargs))
         return ee.Image(result.copyProperties(args[0])\
             .set('system:time_start',args[0].get('system:time_start')))
 
