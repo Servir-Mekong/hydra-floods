960859663d289a59b3fbb4ac8b5ac4b63c32faae
kmarkert <kel.markert@gmail.com>
2020-11-06 11:52:23 -0600

    adding doc strings and reworking decode data function

--- hydrafloods/utils.py
+++ hydrafloods/utils.py
@@ -8,7 +8,22 @@ import numpy as np
 import gcsfs
 
 
-def list_gcs_objs(bucket_path, pattern=None, output_url=False,project=None):
+def list_gcs_objs(bucket_path, pattern=None, output_url=False, project=None):
+    """Function to list objects in Google Cloud Storage Bucket
+
+    args:
+        bucket_path (str): Google Cloud Storage bucket name
+        pattern (str | None, optional): regex pattern to search in bucket. 
+            Can seach folders by adding folder names (i.e. pattern = 'subfolder/*.txt).
+            If None then will not use search pattern. default = None
+        output_url (bool, optional): boolean switch to output google cloud storage http url
+            or google cloud storage object uri. If false will output gcs uri. default = False
+        project (str | None): Cloud project name to use when initiation file spec. If None then
+            use default gcloud config. default = None
+
+    returns:
+        list[str]: List of objects in bucket that match pattern
+    """
     fs = gcsfs.GCSFileSystem(project=project)
     if pattern is not None:
         bucket_path = (
@@ -23,7 +38,15 @@ def list_gcs_objs(bucket_path, pattern=None, output_url=False,project=None):
     return [base.format(blob) for blob in blobs]
 
 
-def push_to_gcs(file, bucketPath):
+def push_to_gcs(file, bucket_path):
+    """Helper function to copy local files to Google Cloud Storage
+    Thinly wraps `gsutil cp` command line
+
+    args:
+        file (str): file path to push to GCS
+        bucket_path (str): path on GCS to copy file to
+
+    """
     if os.path.exists(file):
         cmd = "gsutil cp {0} {1}".format(file, bucketPath)
         proc = subprocess.Popen(
@@ -36,6 +59,17 @@ def push_to_gcs(file, bucketPath):
 
 
 def push_to_ee(bucket_obj, asset_collection, properties=None, delete_bucket_obj=False):
+    """Helper function to begin ingest process for imagery on GCS to GEE
+    Thinly wraps `earthengine upload image`
+
+    args:
+        bucket_obj (str): GCS bucket object to ingest into GEE. Expects that object has mime type of image/tiff
+        asset_collection (str): Earth Engine asset collection to push object to
+        properties (list[str], optional): list of properties to set when ingesting files. If None then no properties
+            will be set. default = None
+        delete_bucket_obj (bool, optional): boolean switch to delete GCS object once ingested into EE. If set to False
+            then file will remain on GCS. default = False
+    """
     name = os.path.basename(bucket_obj).replace(".", "_")
     asset = asset_collection + name
 
@@ -77,34 +111,30 @@ def push_to_ee(bucket_obj, asset_collection, properties=None, delete_bucket_obj=
     return
 
 
-def decode_date(string):
+def decode_date(date):
     """Decodes a date from a command line argument, returning msec since epoch".
-  Args:
-    string: See AssetSetCommand class comment for the allowable
-      date formats.
-  Returns:
-    long, ms since epoch
-  Raises:
-    argparse.ArgumentTypeError: if string does not conform to a legal
-      date format.
-  """
+    
+    args:
+        date (str): date value in a format that can be parsed into datetime object
+        
+    returns:
+        datetime.datetime: decoded datetime value
 
-    try:
-        return int(string)
-    except ValueError:
-        date_formats = [
-            "%Y%m%d",
-            "%Y-%m-%d",
-            "%Y-%m-%dT%H:%M:%S",
-            "%Y-%m-%dT%H:%M:%S.%f",
-        ]
-        for date_format in date_formats:
-            try:
-                dt = datetime.datetime.strptime(string, date_format)
-                return dt
-            except ValueError:
-                continue
-    raise TypeError(
-        'Invalid value for property of type "date": "%s".' % string
-    )
+    raises:
+        TypeError: if string does not conform to a legal date format.
+    """
+
+    date_formats = [
+        "%Y%m%d",
+        "%Y-%m-%d",
+        "%Y-%m-%dT%H:%M:%S",
+        "%Y-%m-%dT%H:%M:%S.%f",
+    ]
+    for date_format in date_formats:
+        try:
+            dt = datetime.datetime.strptime(date, date_format)
+            return dt
+        except ValueError:
+            continue
+    raise TypeError(f"Invalid value for property of type 'date': '{date}'.")
 
