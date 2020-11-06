201dcaf298094d13aa336e2dd8725e02bb279590
kmarkert <kel.markert@gmail.com>
2020-11-06 12:13:38 -0600

    adding install_requires to setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='hydrafloods',
-      version='0.2.1',
-      description='HYDrologic Remote sensing Analysis for Floods',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='http://github.com/servir-mekong/hydra-floods',
-      packages=setuptools.find_packages(),
-      author='Kel Markert',
-      author_email='kel.markert@gmail.com',
-      license='GNU GPL v3.0',
-      zip_safe=False,
-      include_package_data=True,
-      entry_points={
-        'console_scripts': [
-            'hydrafloods = hydrafloods.hfcli:main',
-        ],
-    },)
+    version='0.2.1',
+    description='HYDrologic Remote sensing Analysis for Floods',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/servir-mekong/hydra-floods',
+    packages=setuptools.find_packages(),
+    author='Kel Markert',
+    author_email='kel.markert@gmail.com',
+    license='GNU GPL v3.0',
+    zip_safe=False,
+    include_package_data=True,
+    entry_points={
+    'console_scripts': [
+        'hydrafloods = hydrafloods.hfcli:main',
+    ]},
+    install_requires=[
+        'simplecmr'
+        'earthengine-api',
+    ],
+)
