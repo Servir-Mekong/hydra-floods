23c40b28dacd6240c95fb52091a17de2a1a872ee
kmarkert <kel.markert@gmail.com>
2020-11-18 14:04:11 -0600

    implemented illumination corrections for optical data

--- hydrafloods/corrections.py
+++ hydrafloods/corrections.py
@@ -1,12 +1,13 @@
 import ee
 import math
-from hydrafloods import geeutils
+from hydrafloods import geeutils, decorators
 
 
+@decorators.carry_metadata
 def slope_correction(image, elevation, model="volume", buffer=0, scale=1000):
     """This function applies the slope correction on a Sentinel-1 image.
-    Function based on https://doi.org/10.3390/rs12111867.
-    Adapted from https://github.com/ESA-PhiLab/radiometric-slope-correction/blob/master/notebooks/1%20-%20Generate%20Data.ipynb
+    Function based on https:# doi.org/10.3390/rs12111867.
+    Adapted from https:# github.com/ESA-PhiLab/radiometric-slope-correction/blob/master/notebooks/1%20-%20Generate%20Data.ipynb
        
     args:
         image (ee.Image): Sentinel-1 to perform terrain flattening
@@ -58,7 +59,7 @@ def slope_correction(image, elevation, model="volume", buffer=0, scale=1000):
         return nominator.divide(denominator)
 
     def _erode(image, distance):
-        """Closure functio nto buffer raster values
+        """Closure function to buffer raster values
 
         args:
             image (ee.Image): image that should be buffered
@@ -71,7 +72,7 @@ def slope_correction(image, elevation, model="volume", buffer=0, scale=1000):
         d = (
             image.Not()
             .unmask(1)
-            .fastDistanceTransform(30)
+            .fastDistanceTransform(10)
             .sqrt()
             .multiply(ee.Image.pixelArea().sqrt())
         )
@@ -165,7 +166,9 @@ def slope_correction(image, elevation, model="volume", buffer=0, scale=1000):
     # 2.2
     # Gamma_nought
     gamma0 = sigma0Pow.divide(theta_iRad.cos())
-    gamma0dB = geeutils.db_to_power(gamma0).select(["VV","VH"],["VV_gamma0", "VH_gamma0"])
+    gamma0dB = geeutils.db_to_power(gamma0).select(
+        ["VV", "VH"], ["VV_gamma0", "VH_gamma0"]
+    )
 
     if model == "volume":
         scf = _volumetric_model_SCF(theta_iRad, alpha_rRad)
@@ -175,12 +178,144 @@ def slope_correction(image, elevation, model="volume", buffer=0, scale=1000):
 
     # apply model for Gamm0_f
     gamma0_flat = gamma0.divide(scf)
-    gamma0_flatDB = geeutils.power_to_db(gamma0_flat).select(["VV","VH"],['VV_flat',"VH_flat"])
+    gamma0_flatDB = geeutils.power_to_db(gamma0_flat).select(
+        ["VV", "VH"], ["VV_flat", "VH_flat"]
+    )
 
     # calculate layover and shadow mask
     masks = _masking(alpha_rRad, theta_iRad, buffer)
 
-    return (gamma0_flatDB.updateMask(masks)
+    return (
+        gamma0_flatDB.updateMask(masks)
         .addBands(image.select("angle"))
         .addBands(theta_liaDeg.rename("local_inc_angle"))
     )
+
+
+@decorators.carry_metadata
+def illumination_correction(img, elevation, model="rotation", scale=90, sensor="LC8"):
+    """This function applies a terrain correction to optical imagery based on solar and viewing geometry
+     
+    args:
+        image (ee.Image): Optical image to perform terrain flattening
+        elevation (ee.Image): Input DEM to calculate slope corrections from
+        model (str, optional): correction model to be applied. Options are 'rotation' or 'scs+c'.
+            default = 'scs+c'
+        scale (int, optional): reduction scale to process satellite heading compared to ground. Increasing will reduce
+            chance of OOM errors but reduce local scale correction accuracy. default = 90
+        sensor (str, optional): name of sensor to correct. 
+        
+    returns:
+        ee.Image: terrain flattened optical imagery
+    """
+    def _get_band_coeffs(band_name):
+        """Closure function to find illumination correction fit across the different bands
+
+        args:
+            band_name (str | ee.String): band name to find correction coefficients for
+        """
+
+        #  Create the image to apply the linear regression.The first band
+        # is the cosi and the second band is the response variable, the reflectance (the bands).
+        # L (y) = a + b*cosi(x); a = intercept, b = slope
+        #  Dependent: Reflectance
+        y = img.select([band_name])
+        #  create an image with the three variables by concatenating them
+        reg_img = ee.Image.cat([cosi, one, y])
+        #  specify the linear regression reducer
+        lr_reducer = ee.Reducer.linearRegression(numX=2, numY=1)
+        #  fit the model
+        fit = reg_img.reduceRegion(
+            reducer=lr_reducer, geometry=img.geometry(), scale=scale, maxPixels=1e10
+        )
+
+        #  Get the coefficients as a nested list, cast it to an array, and get
+        #  just the selected column
+        slope = ee.Array(fit.get("coefficients")).get([0, 0])
+        intercept = ee.Array(fit.get("coefficients")).get([1, 0])
+
+        # Calculate C parameter C= a/b
+        # C = int.divide(slo)
+
+        return ee.List([slope,intercept])
+
+    if sensor.lower() == "lc8":
+        sz_property = "SOLAR_ZENITH_ANGLE"
+        sa_property = "SOLAR_AZIMUTH_ANGLE"
+    elif sensor.lower() == "s2":
+        sz_property = "MEAN_SOLAR_ZENITH_ANGLE"
+        sa_property = "MEAN_SOLAR_AZIMUTH_ANGLE"
+    else:
+        raise NotImplementedError()
+
+    # value convert angle to radians
+    to_radians = ee.Number((math.pi / 180))
+    # constand image of 1
+    one = ee.Image.constant(1).rename("one")
+
+    # calculate terrain info from elevation data
+    terrain = ee.Algorithms.Terrain(elevation)
+    # Extract slope in radians for each pixel in the image
+    p = terrain.select(["slope"]).multiply(to_radians)
+    # Extract aspect in radians for each pixel in the image
+    o = terrain.select(["aspect"]).multiply(to_radians)
+    # Extract solar zenith angle from the image
+    z = ee.Image.constant(ee.Number(img.get(sz_property)).multiply(to_radians))
+    # Extract solar azimuth from the image
+    az = ee.Image.constant(
+        ee.Number(img.get(sa_property)).multiply(to_radians)
+    )
+
+    cosao = (o.subtract(az)).cos()
+    # cos(ϕa−ϕo)
+    # Calculate the cosine of the local solar incidence for every pixel in the image in radians (cosi=cosp*cosz+sinp*sinz*cos(ϕa−ϕo)
+    cosi = img.expression(
+        "((cosp * cosz) + (sinp * sinz * cosao))",
+        {
+            "cosp": p.cos(),
+            "cosz": z.cos(),
+            "sinp": p.sin(),
+            "sinz": z.sin(),
+            "cosao": cosao,
+        },
+    )
+
+    if model == "cosine":
+        # if cosine model correction, return early as we don't need to do extra processing
+        return img.expression(
+            "((img * cosz) / cosi) ", {"img": img, "cosz": z.cos(), "cosi": cosi}
+        )
+    
+    bnames = img.bandNames()
+    ab = ee.Array(bnames.map(_get_band_coeffs))
+
+    # get the coefficients as images
+    a = ee.Image(ee.Array(ab.slice(1,0,1))).arrayProject([0]).arrayFlatten([bnames])
+    b = ee.Image(ee.Array(ab.slice(1,1,2))).arrayProject([0]).arrayFlatten([bnames])
+    C = b.divide(a)
+
+    if model == "c":
+        newimg = img.expression(
+            "((img * (cosz + C)) / (cosi + C))",
+            {"img": img, "cosz": z.cos(), "cosi": cosi, "C": C},
+        )
+
+    elif model == "scs+c":
+        newimg = img.expression(
+            "((img * ((cosp * cosz) + C))/(cosi + C))",
+            {"img": img, "cosp": p.cos(), "cosz": z.cos(), "cosi": cosi, "C": C},
+        )
+
+    elif model == "rotation":
+        # Apply the empirical rotation model
+        newimg = img.expression(
+            "img - a * (cosi - cosz)",
+            {"img": img, "cosz": z.cos(), "cosi": cosi, "a": a},
+        )
+
+    else:
+        raise NotImplementedError(
+            f"defined model: {model}, has not been implemented. Options are 'cosine', 'c', 'scs+c', or 'rotation'"
+        )
+
+    return newimg
