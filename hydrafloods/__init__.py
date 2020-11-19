65f8414b0208e0d68b5524682e65d7e432a5efd9
kmarkert <kel.markert@gmail.com>
2020-11-19 00:01:58 -0600

    patch version bump -> 0.2.10

--- hydrafloods/__init__.py
+++ hydrafloods/__init__.py
@@ -3,7 +3,8 @@ from hydrafloods.geeutils import *
 from hydrafloods.thresholding import *
 from hydrafloods.filtering import *
 from hydrafloods.corrections import *
+from hydrafloods.indices import *
 from hydrafloods import fetch, utils
 # from hydrafloods import *
 
-__version__ = "0.2.9"
+__version__ = "0.2.10"
