ebebf00b34abf1d115b20cc896929f6912f2c674
kmarkert <kel.markert@gmail.com>
2021-10-07 17:03:01 -0600

    adding decorator function to force the same band names as the inputs

--- hydrafloods/decorators.py
+++ hydrafloods/decorators.py
@@ -1,13 +1,13 @@
 import ee
 import functools
 
-
+# keep_attrs
 def carry_metadata(func):
     """Decorator function to set the properties of an image from computations to that of the input
     Function to decorate should take an ee.Image object and return an ee.Image object
 
     args:
-        func (object): function object to wrap. Expects that an element within args is of type 
+        func (object): function object to wrap. Expects that an element within args is of type
             ee.Image and will use first ee.Image to carry metadata
 
     Example:
@@ -31,5 +31,36 @@ def carry_metadata(func):
                 "system:time_start", img.get("system:time_start")
             )
         )
+
+    return wrapper
+
+
+def keep_names(func):
+    """Decorator function to set the band names of an image from computations to that of the input
+    Function to decorate should take an ee.Image object and return an ee.Image object
+
+    args:
+        func (object): function object to wrap. Expects that an element within args is of type
+            ee.Image and will use first ee.Image to carry metadata
+
+    Example:
+        ```python
+        @decorators.keep_names
+        def my_computation(img):
+            const = ee.Image.constant(2)
+            return const.multiply(img)
+        ```
+        Returned image(s) will have the same band names as the input.
+        This should only be used if the input and output images will have the same number of bands!
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # expects an element within args is img
+        # will set the metadata to first ee.Image instance
+        # this assumption is true for 99% of functions used for ee.ImageCollection.map()
+        result = ee.Image(func(*args, **kwargs))
+        img = [i for i in args if isinstance(i, ee.Image)][0]
+        return result.rename(img.bandNames())
 
     return wrapper
